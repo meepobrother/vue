59b6640ba4d71c7f794f13edffaa1fbcdd2e3573
15138730370 <1037483576@qq.com>
2017-12-06 11:35:47 +0800

    save

--- tasks/bump.js
+++ tasks/bump.js
@@ -1,11 +1,56 @@
 var gulp = require('gulp');
 var bump = require('gulp-bump');
+var fs = require('fs');
+var semver = require('semver');
+let filter = require('gulp-filter');
 
-gulp.task('bump', function() {
-    gulp.src('../*.json')
-        .pipe(bump({
-            version: '1.2.3',
-            type: 'major'
-        }))
-        .pipe(gulp.dest('./'));
+var jsonFiles = [
+    './bower.json',
+    './package.json',
+    './src/manifest.json'
+];
+
+var getPackageJson = function() {
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+};
+
+var saveVersion = function(version) {
+    jsonFiles.map(jsonFile => {
+        let package = JSON.parse(fs.readFileSync(jsonFile, 'utf-8'));
+        package.version = version;
+        fs.writeFileSync(jsonFile, JSON.stringify(package), 'utf-8')
+    });
+}
+
+gulp.task('bump.minor', function() {
+    bumpBuilder('minor');
 });
+
+gulp.task('bump.major', function() {
+    bumpBuilder('major');
+});
+
+gulp.task('bump.patch', function() {
+    bumpBuilder('patch');
+});
+
+gulp.task('bump.prerelease', function() {
+    bumpBuilder('prerelease');
+});
+
+function bumpBuilder(type) {
+    var pkg = getPackageJson();
+    var newVer = semver.inc(pkg.version, type);
+    saveVersion(newVer);
+    var manifestFilter = filter(['manifest.json']);
+    var regularJsons = filter(['!manifest.json']);
+    return gulp.src(['./bower.json', './package.json', './src/manifest.json'])
+        .pipe(bump({
+            version: newVer
+        }))
+        .pipe(manifestFilter)
+        .pipe(gulp.dest('./src'))
+        // .pipe(manifestFilter.restore())
+        .pipe(regularJsons)
+        .pipe(gulp.dest('./'));
+}
