460b302f3405a82098da887756b1effbc412debb
15138730370 <1037483576@qq.com>
2017-12-06 13:03:38 +0800

    项目页面功能初始化

--- addons/imeepos_runnerpro/site.php
+++ addons/imeepos_runnerpro/site.php
@@ -11,18 +11,33 @@ class Imeepos_runnerproModuleSite extends WeModuleSite {
 
 	public function doMobileList() {
 		//列表
+		global $_W,$_GPC;
+
+		include $this->template('v2/list');
 	}
 	public function doMobileHome() {
 		//个人中心
+		global $_W,$_GPC;
+		
+		include $this->template('v2/home');
 	}
 	public function doMobilePost() {
 		//发布
+		global $_W,$_GPC;
+		
+		include $this->template('v2/post');
 	}
 	public function doMobileSearch() {
 		//搜索
+		global $_W,$_GPC;
+		
+		include $this->template('v2/search');
 	}
 	public function doMobileDetail() {
 		//详情
+		global $_W,$_GPC;
+		
+		include $this->template('v2/detail');
 	}
 	public function doWebIndex(){
 		include $this->template('index');
