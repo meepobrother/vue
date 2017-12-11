b33373d021219876e3d0e74c68f6708ffe482c3f
15138730370 <1037483576@qq.com>
2017-12-11 11:11:19 +0800

    save

--- coach-post/src/app/app.component.ts
+++ coach-post/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoachService } from './components/coach.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widget: any = {
+    header: this.coach.header,
+    footer: this.coach.footer
+  };
   title = 'app';
+
+  constructor(
+    public coach: CoachService
+  ) { }
+
+  onHeader(type: string) {
+    console.log(type);
+    if (type === 'city') {
+      window.location.href = this.coach.api.getUrl('citys_select', {});
+    }
+    if (type === 'my') {
+      window.location.href = this.coach.api.getUrl('coach_my', {});
+    }
+    if (type === 'post') {
+      window.location.href = this.coach.api.getUrl('coach_add', {});
+    }
+    if (type === 'title') {
+      window.location.href = this.coach.api.getUrl('coach_index', {});
+    }
+  }
+
+  onFooterItem(item: any) {
+    if (item.link) {
+      window.location.href = item.link;
+    }
+  }
 }
